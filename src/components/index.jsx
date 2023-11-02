61dff197bc6ce33f044328facb70bdb8f4f5718b
Mr.Lion <uchechukwuobiakor@gmail.com>
2023-11-02 17:22:17 +0100

    Working on the register page

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,2 +1,3 @@
 export { default as ErrorElement } from './ErrorElement'
 export { default as FormInput } from './FormInput'
+export { default as SubmitBtn } from './SubmitBtn'
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,42 @@
+import { Form, Link } from 'react-router-dom'
+import { FormInput, SubmitBtn } from '../components'
+
 const Login = () => {
-  return <div>Login</div>
+  return (
+    <section className='h-screen grid place-items-center'>
+      <Form
+        method='POST'
+        className=' card w-96  p-8 bg-base-100 shadow-lg flex flex-col gap-y-4'
+      >
+        <h4 className='text-center text-3xl font-bold'>Login</h4>
+        <FormInput
+          type='email'
+          name='identifier'
+          label='email'
+          defaultValue='test@test.com'
+        />
+        <FormInput
+          type='password'
+          name='password'
+          label='password'
+          defaultValue='secret'
+        />
+        <div className='mt-4'>
+          <SubmitBtn text='Login' />
+        </div>
+        <button className='btn btn-secondary btn-block'>Guest User</button>
+        <p>
+          No account yet?{' '}
+          <Link to='/register' className='link link-success'>
+            Register
+          </Link>
+          <Link to='/' className='ms-3 link link-primary'>
+            Back to home
+          </Link>
+        </p>
+      </Form>
+    </section>
+  )
 }
 
 export default Login
