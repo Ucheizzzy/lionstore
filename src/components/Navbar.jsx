faa8ad6ab90bd7619e9ecb4c5fcc9be08de7eabb
Mr.Lion <uchechukwuobiakor@gmail.com>
2023-11-03 15:15:40 +0100

    Working on about page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { FaBarsStaggered } from 'react-icons/fa6'
 import { useDispatch } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import { toggleTheme } from '../features/userSlice'
+import NavLinks from './NavLinks'
 const Navbar = () => {
   const dispatch = useDispatch()
 
@@ -24,31 +25,33 @@ const Navbar = () => {
             </label>
             <ul
               tabIndex={0}
-              className='menu menu-sm dropdown-content mt-3 z-[-1] p-2 shadow-md bg-base-200 rounded-box w-52'
+              className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow-md bg-base-200 rounded-box w-52'
             >
-              navlinks
+              <NavLinks />
             </ul>
           </div>
-          <div className='navbar-center hidden sm:flex'>
-            <ul className='menu menu-horizontal'>navlinks</ul>
-          </div>
-          <div className='navbar-end'>
-            {/* theme icons */}
-            <label className='swap swap-rotate'>
-              <input type='checkbox' onClick={() => dispatch(toggleTheme())} />
-              <BsSunFill className='swap-on h-4 w-4' />
-              <BsMoonFill className='swap-off h-4 w-4' />
-            </label>
-            {/* cart link */}
-            <NavLink to='cart' className='btn btn-ghost btn-circle ml-4'>
-              <div className='indicator'>
-                <BsCart3 className='h-6 w-6' />
-                <span className='badge badge-sm badge-primary indicator-item'>
-                  0
-                </span>
-              </div>
-            </NavLink>
-          </div>
+        </div>
+        <div className='navbar-center hidden sm:flex'>
+          <ul className='menu menu-horizontal'>
+            <NavLinks />
+          </ul>
+        </div>
+        <div className='navbar-end '>
+          {/* theme icons */}
+          <label className='swap swap-rotate'>
+            <input type='checkbox' onClick={() => dispatch(toggleTheme())} />
+            <BsSunFill className='swap-on h-4 w-4' />
+            <BsMoonFill className='swap-off h-4 w-4' />
+          </label>
+          {/* cart link */}
+          <NavLink to='/cart' className='btn btn-ghost btn-circle btn-md ml-4'>
+            <div className='indicator'>
+              <BsCart3 className='h-6 w-6' />
+              <span className='badge badge-sm badge-primary indicator-item'>
+                0
+              </span>
+            </div>
+          </NavLink>
         </div>
       </div>
     </nav>
