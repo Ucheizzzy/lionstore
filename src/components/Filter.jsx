90db550df9c2b355d4457636ca6fe70e4ced1f29
Mr.Lion <uchechukwuobiakor@gmail.com>
2023-11-06 21:00:14 +0100

    working on pagination

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,12 +1,13 @@
-import { Form, useLoaderData } from 'react-router-dom'
+import { Form, useLoaderData, Link } from 'react-router-dom'
 import FormInput from './FormInput'
 import FormSelect from './FormSelect'
 import FormRange from './FormRange'
 import FormCheckBox from './FormCheckBox'
 
 const Filter = () => {
-  const { meta } = useLoaderData()
-  // console.log(meta)
+  const { meta, params } = useLoaderData()
+  // console.log(params)
+  const { search, category, company, shipping, order, price } = params
   return (
     <Form className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
       <FormInput
@@ -14,34 +15,48 @@ const Filter = () => {
         label='search products'
         name='search'
         size='input-sm'
+        defaultValue={search}
       />
       <FormSelect
         list={meta.companies}
         label='select company'
         name='company'
         size='select-sm'
+        defaultValue={company}
       />
       <FormSelect
         list={meta.categories}
         label='select category'
         name='category'
         size='select-sm'
+        defaultValue={category}
       />
       <FormSelect
         list={['a-z', 'z-a', 'high', 'low']}
         label='sort by'
         name='order'
         size='select-sm'
+        defaultValue={order}
       />
-      <FormRange label='select price' name='price' size='range-sm' />
-      <FormCheckBox label='free shipping' name='shipping' size='checkbox-sm' />
+      <FormRange
+        label='select price'
+        name='price'
+        size='range-sm'
+        price={price}
+      />
+      <FormCheckBox
+        label='free shipping'
+        name='shipping'
+        size='checkbox-sm'
+        defaultValue={shipping}
+      />
 
       <button type='submit' className='btn btn-primary btn-sm'>
         Search
       </button>
-      <button type='submit' className='btn btn-accent btn-sm'>
+      <Link to='/products' type='submit' className='btn btn-accent btn-sm'>
         Reset
-      </button>
+      </Link>
     </Form>
   )
 }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,19 +1,38 @@
 import { Filter, ProductsContainer, PaginationContainer } from '../components'
 import { customFetch } from '../utils'
 
-const allProductsQuery = () => {
+const allProductsQuery = (queryParams) => {
+  const { search, category, company, sort, price, shipping, page } = queryParams
   return {
-    queryKey: ['products'],
-    queryFn: () => customFetch.get('/products'),
+    queryKey: [
+      'products',
+      search ?? '',
+      category ?? 'all',
+      company ?? 'all',
+      sort ?? 'a-z',
+      price ?? 100000,
+      shipping ?? false,
+      page ?? 1,
+    ],
+    queryFn: () =>
+      customFetch.get('/products', {
+        params: queryParams,
+      }),
   }
 }
-export const loader = (queryClient) => async () => {
-  const { data } = await queryClient.ensureQueryData(allProductsQuery())
-  const products = data.data
-  const meta = data.meta
-  // console.log(data)
-  return { products, meta }
-}
+export const loader =
+  (queryClient) =>
+  async ({ request }) => {
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ])
+
+    const { data } = await queryClient.ensureQueryData(allProductsQuery(params))
+    const products = data.data
+    const meta = data.meta
+    // console.log(data)
+    return { products, meta, params }
+  }
 
 const Products = () => {
   return (
