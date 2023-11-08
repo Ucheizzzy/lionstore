bd8e87b12fea23f28f4f4a3f3e157594cc7709f9
Mr.Lion <uchechukwuobiakor@gmail.com>
2023-11-08 18:30:33 +0100

    working on the user slice for login and logout

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,63 @@
+import { useDispatch } from 'react-redux'
+import { formatPrice, generateAmountOptions } from '../utils'
+import { editItem, removeItem } from '../features/cartSlice'
+
+const CartItem = ({ cartItem }) => {
+  const { cartID, title, price, image, amount, company, productColor } =
+    cartItem
+  const dispatch = useDispatch()
+
+  const handleAmount = (e) => {
+    dispatch(editItem({ cartID, amount: parseInt(e.target.value) }))
+  }
+  return (
+    <article
+      key={cartID}
+      className='mb-12 flex flex-col gap-y-4 sm:flex-row flex-wrap border-b border-base-300 pb-6 last:border-b-0'
+    >
+      <img
+        src={image}
+        alt={title}
+        className='h-24 w-24 rounded-lg sm:h-32 sm:w-32 object-cover'
+      />
+      {/* info */}
+      <div className='sm:ml-16 sm:w-48'>
+        <h4 className='capitalize font-medium'>{title}</h4>
+        <h4 className='mt-2 capitalize text-sm text-neutral-500'>{company}</h4>
+        <p className='mt-4 text-sm capitalize flex items-center gap-x-2'>
+          Color:
+          <span
+            className='badge badge-sm'
+            style={{ backgroundColor: productColor }}
+          ></span>
+        </p>
+      </div>
+      <div className='sm:ml-12'>
+        <div className='form-control max-w-xs'>
+          <label htmlFor='amount' className='label p-0'>
+            <span className='label-text'>Amount</span>
+          </label>
+          <select
+            name='amount'
+            id='amount'
+            className='mt-2 select select-base select-bordered select-xs'
+            value={amount}
+            onChange={handleAmount}
+          >
+            {generateAmountOptions(amount + 1)}
+          </select>
+        </div>
+
+        <button
+          onClick={() => dispatch(removeItem({ cartID }))}
+          className='mt-2 link link-primary link-hover text-sm'
+        >
+          remove
+        </button>
+      </div>
+      <p className='font-medium sm:ml-auto'> {formatPrice(price)}</p>
+    </article>
+  )
+}
+
+export default CartItem
